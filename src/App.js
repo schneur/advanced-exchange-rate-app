077192762bb3f9c26abcc0529f0014ad036432b0
Schneur <schneuryiftach@gmail.com>
2021-10-03 15:22:44 -0400

    chart added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-// App.js
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Layout from './Layout';
 import Home from './Home';
+import CurrencyConverter from './CurrencyConverter';
 
 import './App.css';
 
@@ -11,7 +11,8 @@ const App = () => {
     <Router>
       <Layout>
         <Switch>
-          <Route path="/" exact component={Home}/> 
+          <Route path="/" exact component={Home} />
+          <Route path="/currencyconverter" component={CurrencyConverter} />
           <Route render={() => <h1>404 Not found</h1>} />
         </Switch>
       </Layout>
