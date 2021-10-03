58b0a98d84fb9a4507f040d5d0fbf27da571fd20
Schneur <schneuryiftach@gmail.com>
2021-10-03 13:05:37 -0400

    nav,footer

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
+// App.js
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Layout from './Layout'
+
 import './App.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Layout>
+        <Switch>
+          <Route path="/" exact render={() => <h1>Hello world</h1>} />
+          <Route render={() => <h1>404 Not found</h1>} />
+        </Switch>
+      </Layout>
+    </Router>
   );
 }
 
